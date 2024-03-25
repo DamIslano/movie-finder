f4f42e5f2fd9c47200f981a354d1e2d39c5d5676
Ivan <vanekswmpai@gmail.com>
2024-03-25 20:24:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Movie Finder
+
+Here you can find a movie to watch, learn information about a particular film, and there is functionality provided for uploading your own movie to the site.
+
+#
+
+![cv-prev](demo/chrome_fURcSofgXV.gif)
+
+### Project setup
+
+```
+$ npm install
+$ npm run dev
+```
---
+++ pages/results/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useMoviesStore } from '../../stores/movies';
+
+const store = useMoviesStore()
+
+</script>
+
+<template>
+  <div>
+    <BaseMovie :movie="[store.currentMovie]" />
+    <div class="pl-10 text-3xl text-slate-200 py-5">We Also Recommend</div>
+    <BaseMovieList />
+  </div>
+</template>
